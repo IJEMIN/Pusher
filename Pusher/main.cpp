b030d2febac2175802e62ad670c31a8f9d38eab5
I_Jemin <I_Jemin@I-Jeminui-MacBook-Pro.local>
2015-02-19 04:57:32 +0900

    Initial Commit

---
+++ Pusher/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pusher
+//
+//  Created by I_Jemin on 2015. 2. 19..
+//  Copyright (c) 2015년 I_Jemin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
